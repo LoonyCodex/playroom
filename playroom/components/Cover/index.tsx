733039d4f443402aed563f934209e955aa2e94c8
Poly Devil <polydevil@cocaine.ninja>
2022-01-10 09:00:38 +0300

    Update `<Cover />`

--- playroom/components/Cover/index.tsx
+++ playroom/components/Cover/index.tsx
@@ -6,21 +6,21 @@ export default [
   {
     group: Cover.displayName,
     name: '<section>: Обложка',
-    code: render(
-      <Cover image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
+    code: `
+      <${Cover.displayName} image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
         <H1>Центрированный заголовок</H1>
-      </Cover>
-    ),
+      </${Cover.displayName}>
+    `,
   },
   {
     group: Cover.displayName,
     name: '<section>: Обложка с датой',
-    code: render(
-      <Cover image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
+    code: `
+      <${Cover.displayName} image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
         <Time>04.12.1995</Time>
         <H1>Центрированный заголовок</H1>
-      </Cover>
-    ),
+      </${Cover.displayName}>
+    `,
   },
 ]
 
