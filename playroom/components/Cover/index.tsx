e7190605cfe92aff74a3afd6aa9fd59341be9b21
Poly Devil <polydevil@cocaine.ninja>
2022-01-12 09:07:59 +0300

    update `<Cover />`

--- playroom/components/Cover/index.tsx
+++ playroom/components/Cover/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Cover, H1, Time } from '@ProtoSetUI/set-ui'
 import render from 'react-element-to-jsx-string'
 
 export default [
@@ -7,19 +6,19 @@ export default [
     group: Cover.displayName,
     name: '<section>: Обложка',
     code: `
-      <${Cover.displayName} image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
+      <Cover image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
         <H1>Центрированный заголовок</H1>
-      </${Cover.displayName}>
+      </Cover>
     `,
   },
   {
     group: Cover.displayName,
     name: '<section>: Обложка с датой',
     code: `
-      <${Cover.displayName} image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
+      <Cover image="https://upload.wikimedia.org/wikipedia/commons/d/d6/Dresden_Codex_pp.58-62_78.jpg">
         <Time>04.12.1995</Time>
         <H1>Центрированный заголовок</H1>
-      </${Cover.displayName}>
+      </Cover>
     `,
   },
 ]
